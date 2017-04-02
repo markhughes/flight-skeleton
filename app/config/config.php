3c9698e7ff89933e5b66875a260d4720d5961f65
Mark Hughes <m@rkhugh.es>
2017-04-02 17:24:21 +1000

    Adding Monolog

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,30 @@
 <?php
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+
+// -------------------------------------------------- //
+// APP CONFIG
+// -------------------------------------------------- //
+
+define("APP_NAME", "my_app");
+define("DEBUG", true);
+
 // -------------------------------------------------- //
 // FLIGHT CONFIG
 // -------------------------------------------------- //
 
 Flight::set("flight.views.path", __DIR__ . "/../resources/views/");
+
+// -------------------------------------------------- //
+// MONOLOG
+// -------------------------------------------------- //
+
+$logger = new Logger(APP_NAME);
+
+$logger->pushHandler(new StreamHandler(__DIR__ . "/../logs/application.log", Logger::DEBUG));
+$logger->pushHandler(new FirePHPHandler());
 
 // -------------------------------------------------- //
 // ROUTES
