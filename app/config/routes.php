afa63490c4532baaaeaa89df89be6f64785d2a4b
Mark Hughes <markwilliamhughes1@gmail.com>
2022-12-01 16:53:13 +1100

    fix: resolve 404 page issues

--- app/config/routes.php
+++ app/config/routes.php
@@ -11,6 +11,6 @@ Flight::route("/api/message", ["\Acme\Demo\Controller\API", "message"]);
 // MAPPINGS
 // -------------------------------------------------- //
 
-Flight::map("notFound", fn() => Flight::render("404", []));
+Flight::map("notFound", fn() => Flight::view()->display("404.twig", []));
 
 Flight::map("error", fn(Throwable $exception) => $logger->error($exception));
